1a66da294c8590b6d7ac7dadb2289f5a5e82f0d5
PianoMan0 <180116946+PianoMan0@users.noreply.github.com>
2025-09-07 14:46:30 -0400

    Folder fix againnn

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ProductCard from '../../components/ProductCard';
+import ProductCard from './components/ProductCard';
 import './main.css';
 
 const Home = () => {
